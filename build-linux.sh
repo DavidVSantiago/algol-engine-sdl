05e275697ec2f0a920af824fd999d52e65cc52d0
DavidVSantiago <david.valente.santiago@gmail.com>
2024-03-27 13:31:36 -0300

    update

--- build-linux.sh
+++ build-linux.sh
@@ -4,12 +4,12 @@ OUTPUT=bin/linux/main
 SDL_INCLUDE=-IC:/usr/include/SDL2
 SDL_LIB=-LC:/usr/lib/x86_64-linux-gnu
 
-# Lê todas as linhas do arquivo sources.txt
+# Lê todas as linhas do arquivo compile_sources.txt
 while read -r linha; do
   SOURCES="$SOURCES $linha"
 done < compile_sources.txt
 
-# Lê todas as linhas do arquivo parametrosSDL.txt
+# Lê todas as linhas do arquivo compile_SDLparams.txt
 while read -r linha; do
   PARAMS="$PARAMS $linha"
 done < compile_SDLparams.txt
--- build-windows.bat
+++ build-windows.bat
@@ -1,1 +1,18 @@
-g++ -fdiagnostics-color=always -g src/main.cpp src/engine/Engine.h -o bin/windows/main -IC:\SDL2\x86_64-w64-mingw32\include -LC:\SDL2\x86_64-w64-mingw32\lib -lmingw32 -lSDL2main -lSDL2 -mwindows
+@echo off
+COMPILER=g++
+OUTPUT=bin/windows/main
+SDL_INCLUDE=-IC:/usr/include/SDL2
+SDL_LIB=-LC:/usr/lib/x86_64-linux-gnu
+
+rem Lê todas as linhas do arquivo compile_sources.txt
+FOR /F "delims=" %%i IN ('type compile_sources.txt') DO (
+    rem Concatena a linha à variável
+    SET SOURCES=%SOURCES% %%i
+)
+rem Lê todas as linhas do arquivo compile_SDLparams.txt
+FOR /F "delims=" %%i IN ('type compile_SDLparams.txt') DO (
+    rem Concatena a linha à variável
+    SET PARAMS=%PARAMS% %%i
+)
+
+$COMPILER -fdiagnostics-color=always -g %SOURCES% -o $OUTPUT $SDL_INCLUDE $SDL_LIB %PARAMS% -lmingw32 -mwindows
